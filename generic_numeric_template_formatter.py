81dc8dda1f3ce34c49b1adb0c31e03a3305478d2
ashish8796 <ashishsaini.ak@gmail.com>
2019-10-29 23:46:14 +0530

    generic numeric template formatter problem added

---
+++ generic_numeric_template_formatter.py
@@ -0,0 +1,15 @@
+template = "xxx xxxxx xx","5465253289"
+print(template[0], template[1])
+
+
+def numeric_formatter(template):
+    # if len(template[1]) > 1:
+    template = list(template)
+    lst = []
+    for i in template[0].split(' '):
+        lst.append(template[1][:len(i)])
+        template[1] = template[1].replace(template[1][:len(i)], '')
+    print(lst)
+    return ' '.join(lst)
+
+print(numeric_formatter(template))
